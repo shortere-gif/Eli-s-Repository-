46d878e2f26c2bb9a5d6e665575762c4799ba71e
shortere-gif <shortere@stu.otc.school.nz>
2026-05-01 09:58:45 +1200

    make a for loop

--- text.py
+++ text.py
@@ -4,8 +4,11 @@ Points = 0
 question1 = input("What nickname does Paula use to describe Ricky to Bella? \n " \
                   " A) Walking whale B) Tupac  C) A bad egg \n"  )
 
-if question1 == "C":
-  Points + 1
+       try:
+         if question1 == "C":
+           Points + 1
+
+          
 
 
 
