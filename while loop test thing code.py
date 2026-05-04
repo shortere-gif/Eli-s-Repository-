49477ea24617e56b9632fde5289ea8f323bf6dea
shortere-gif <shortere@stu.otc.school.nz>
2026-05-05 10:59:59 +1200

    wilder people good 1

--- while loop test thing code.py
+++ while loop test thing code.py
@@ -2,21 +2,25 @@
 int = maxscore = 9
 int = Points = 0
 int= incorrect = 0
-hunt_list = []
-# This part asks the question
-question1 = input("What nickname does Paula use to describe Ricky to Bella?\n" \
-                 "A) Walking whale B) Tupac  C) A bad egg \n"  )
-# Adds a points a point to the correct score or wrong score
-if question1 == 'C':
-    Points += 1
-else:
+index = 0
+# This list asks all the questions
+questions_list = ["1. What nickname does Paula use to describe Ricky to Bella?\n\
+                  A) Walking whale B) Tupac  C) A bad egg \n",
+                 "2.What was the name of Rickys father figure \n"
+                 "A) Hector B)  Josh  C)  Rick \n",
+                 "3. What movie was referenced? \n"
+                 "A) The Muppets B) lord of the rings C) Terminator\n",]
+# This list has all the answer to see it you were right
+answer_list = ["C)", "A)", "C)"]
+
+for question in questions_list:
+   answer = input(question)
+   answer = answer.upper()
+if answer == questions_list[index]:
+       index = index + 1
+       Points += 1
+else: 
     incorrect += 1
-
-
-
-
-
-
 
 
 
