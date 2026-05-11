64900d5cb333ae101c96cccc3dcc4c95e9a21b57
shortere-gif <shortere@stu.otc.school.nz>
2026-05-11 13:29:49 +1200

    add code to stop incorrect answers

---
+++ ''' This is a quiz about Hunt of the wil.py
@@ -0,0 +1,20 @@
+''' This is a quiz about Hunt of the wilderpeople ''' 
+maxscore = 9
+Points = 0
+question1 = input("What nickname does Paula use to describe Ricky to Bella? \n " \
+                  " A) Walking whale B) Tupac  C) A bad egg \n"  )
+
+if question1 == "C":
+  Points + 1
+
+
+
+
+
+
+
+
+
+
+
+
